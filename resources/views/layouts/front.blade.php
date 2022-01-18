84d4ed5e2d9110b3c0937e2e9b3bb3671b391514
Mikhail A <mg@abramishvili.net>
2022-01-18 14:51:13 +0500

    .

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -9,6 +9,14 @@
         
         <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
         <link href="{{ mix('css/front.css') }}" type="text/css" rel="stylesheet"/>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#603cba">
+        <meta name="theme-color" content="#ffffff">
     </head>
     <body>
         <div id="app">
