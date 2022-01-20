515cccbb9b48d50470a292e14772b654694f3404
Mikhail A <mg@abramishvili.net>
2022-01-20 12:12:18 +0500

    .

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -14,8 +14,8 @@ class SettingSeeder extends Seeder
         DB::table('settings')->insert([
             'id' => 1,
             'tel' => '+7 999 123-45-67',
-            'email' => '1@1.net',
-            'email_for_notifications' => '1@1.net',
+            'email' => 'mining.ferma102@gmail.com',
+            'email_for_notifications' => 'mining.ferma102@gmail.com',
             'address' => 'г. Уфа, ул. Уличная, 1/1, офис 1',
             'logo' => '/img/logo.svg',
             'vk' => 'https://www.youtube.com/channel/UC52vd2JAxC8Nn-G8PYjVv5w/videos',
